a7d3edaef153a4f53906ba7cbae1eaf995cdfba6
rajeshwar <iitkgprajeshwar@gmail.com>
2025-10-01 20:38:36 +0530

    Update range maintenance solution

--- range_maintenance.cpp
+++ range_maintenance.cpp
@@ -1,3 +1,18 @@
+/*Range Maintenance Ideas - 1
+Problem Statement:
+We are given N ranges varies from Li to Ri on number line. Then you are given Q queries in form of ? Y, which means you have to return how many ranges passes through this points. Constraints N, Q <=1e5 Li, Ri, Y <=1e9
+
+Approach:
+To efficiently solve the problem of counting the number of ranges that pass through a given point Y, we can use a binary search-based approach. We'll preprocess the given ranges to sort them based on their left and right endpoints. Then, for each query point Y, we'll use binary search to find the number of ranges that pass through Y.
+
+Here's the detailed approach:
+
+Sort the given ranges based on their left endpoints.
+For each query point Y:
+Perform a binary search to find the number of ranges whose right endpoint is less than Y (i.e., ranges ending before Y). This can be done using the lower_bound function in C++ or the bisect_left function in Python.
+Perform a binary search to find the number of ranges whose left endpoint is greater than Y (i.e., ranges starting after Y). This can be done using the upper_bound function in C++ or the bisect_right function in Python.
+The total number of ranges passing through Y is given by: Total ranges - (number of ranges ending before Y + number of ranges starting after Y).*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
